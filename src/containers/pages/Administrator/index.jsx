7f85bb3570c374a61c7d6ae354bc65d053fc7783
andinaeta <mikorlk4328@gmail.com>
2020-05-08 16:58:29 +0700

    styling

--- src/containers/pages/Administrator/index.jsx
+++ src/containers/pages/Administrator/index.jsx
@@ -7,24 +7,20 @@ import dummy from '../../../assets/img/dummy_general.jpg';
 function Administrator(props) {
     return (
         <Container className="g-mt-60 py-60">
-            <Row>
-                <Col lg={12}>
-                    <Row md={12} className="justify-content-center">
-                        <div className="mx-3">
-                            <BoxCover
-                                image={dummy}
-                                title="Andi Naeta"
-                                description="Situs ini"
-                                onClick={() => props.history.push('/_rdn/administrator/andinaeta')} />
-                        </div>
-                        <div className="mx-3">
-                            <BoxCover
-                                image={dummy}
-                                title="Naeta Store"
-                                description="Aplikasi E-commerce"
-                                onClick={() => props.history.push('/_rdn/administrator/naetastore')} />
-                        </div>
-                    </Row>
+            <Row className="justify-content-center">
+                <Col lg={4}>
+                    <BoxCover
+                        image={dummy}
+                        title="Andi Naeta"
+                        description="Situs ini"
+                        onClick={() => props.history.push('/_rdn/administrator/andinaeta')} />
+                </Col>
+                <Col lg={4}>
+                    <BoxCover
+                        image={dummy}
+                        title="Naeta Store"
+                        description="Aplikasi E-commerce"
+                        onClick={() => props.history.push('/_rdn/administrator/naetastore')} />
                 </Col>
             </Row>
         </Container>
