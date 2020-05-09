8b3947849cf1cfcdde324ec55db020f8a4edca48
andinaeta <mikorlk4328@gmail.com>
2020-05-09 14:20:58 +0700

    refactoring code

--- src/config/REST.js
+++ src/config/REST.js
@@ -1,8 +1,13 @@
 import config from '../config.json';
 
+const localsrv = {
+	'naetastore': 'http://localhost/rest/',
+	'andinaeta': 'http://localhost/andi-rest-server/'
+}
+
 const server = {
-    'naetastore': config.production ? config.rest.server.naetastore : 'http://localhost/rest/',
-    'andinaeta': config.production ? config.rest.server.andinaeta : 'http://localhost/andi-rest-server/'
+    'naetastore': config.production ? config.rest.server.naetastore : localsrv.naetastore,
+    'andinaeta': config.production ? config.rest.server.andinaeta : localsrv.andinaeta
 }
 
 export const REST = { server };
